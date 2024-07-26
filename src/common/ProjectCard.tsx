29b2896dc2d355f357ef0043eeb21d1ea5803124
hui <huiwxng@gmail.com>
2024-07-26 03:44:02 -0400

    fix error with implicit typing

--- src/common/ProjectCard.tsx
+++ src/common/ProjectCard.tsx
@@ -1,6 +1,13 @@
-function ProjectCard({ src, link, title, desc }) {
+interface ProjectCardProps {
+  src: string;
+  link: string;
+  title: string;
+  desc: string;
+}
+
+function ProjectCard({ src, link, title, desc }: ProjectCardProps) {
   return (
-    <a href={link} target="_blank">
+    <a href={link} target="_blank" rel="noopener noreferrer">
       <img className="hover" src={src} alt={`${title} logo`} />
       <h3>{title}</h3>
       <p>{desc}</p>
